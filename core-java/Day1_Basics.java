77f3dad1e7531908118a631af2e820fab9f667c1
Mohammed Arhaan <mohammedarhaan07@gmail.com>
2026-02-06 17:38:47 +0530

    Day 1: Added basics with variables and printing

---
+++ core-java/Day1_Basics.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "arhaan";
+        int age = 22;
+        double percentage = 98.0;
+        System.out.println("Name: " +name );
+        System.out.println("Age: "+age );
+        System.out.println("Percentage: " +percentage );
+        System.out.println("10" +20 );
+        System.out.println(10 +20 );
+    }
+}
